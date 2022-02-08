15af8d5a4529d2267c950a629bf0f820a736b8c9
vkpatil <vishalvkpatil8@gmail.com>
2022-02-08 19:27:43 +0530

    merge

--- address_book_main.py
+++ address_book_main.py
@@ -10,6 +10,7 @@ print("welcome in Address Book")
 
 
 class Contact:
+    contactlist = {}
 
     def __init__(self):
         """
@@ -23,7 +24,37 @@ class Contact:
         self.zipcode = input("Enter zipcode: ")
         self.phonenumber = input("Enter phone number: ")
         self.email = input("Enter email: ")
+        self.addcontact()
+
+    def addcontact(self):
+        Contact.contactlist[self.firstname] = {"1": self.firstname, "2": self.lastname,
+                                               "3": self.address, "4": self.city, "5": self.state,
+                                               "6": self.zipcode, "7": self.phonenumber,
+                                               "8": self.email}
 
 
-if __name__ == '__main__':
-    Contact()
+class EditContact:
+
+    def __init__(self, name):
+        self.name = name
+        self.editcontact(name)
+
+    def editcontact(self, name):
+        a = Contact.contactlist[name]
+        print(a)
+        while True:
+            changeoption = int(input("Select where u want to change \n1:firstname\n2:lastname\n3:address\n4:city\n5"
+                                     ":state\n6:zipcode\n7:phonenumber\n8:email\n9:exit\n"))
+            if changeoption == 9:
+                break
+            value = input("Enter new: ")
+            Contact.contactlist[name][changeoption] = value
+
+
+class ShowContact:
+    def __init__(self, name):
+        self.name = name
+        self.showdetails(name)
+
+    def showdetails(self, name):
+        print(Contact.contactlist[name])
