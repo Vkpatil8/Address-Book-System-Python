dbdf934244ea4b3cb9e3f13f7a8dc3db672fe2e3
vkpatil <vishalvkpatil8@gmail.com>
2022-02-08 11:27:50 +0530

    [vishal] Added: create contact in address book

--- address_book_main.py
+++ address_book_main.py
@@ -1,9 +1,26 @@
 """
 @Author: Vishal Patil
 @Date: 08-02-2022 11-00-00
-@Title : Address Book System Problem
+@Last Modified by: Vishal Patil
+@Last Modified time: 08-02-2022 11:20:00
+@Title : solve use case 1
 """
 
+print("welcome in Address Book")
 
-class AddressBookMain:
-    print("welcome in Address Book")
+
+class Contact:
+
+    def __init__(self, firstname, lastnames, address, city, state, zipcode, phonenumber, email):
+        self.firstname = firstname
+        self.lastnames = lastnames
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.phonenumber = phonenumber
+        self.email = email
+
+
+if __name__ == '__main__':
+    Contact("vishal", "Patil", "Limb", "Limb", "Maharashtra", "416312", "8329235763", "Vk@gmail.com")
