f481a8833e0762feee148abe4bd6ece4d4b06956
vkpatil <vishalvkpatil8@gmail.com>
2022-02-08 11:46:43 +0530

    [vishal] Added: use case for create contact in address book using console

--- address_book_main.py
+++ address_book_main.py
@@ -2,8 +2,8 @@
 @Author: Vishal Patil
 @Date: 08-02-2022 11-00-00
 @Last Modified by: Vishal Patil
-@Last Modified time: 08-02-2022 11:20:00
-@Title : solve use case 1
+@Last Modified time: 08-02-2022 11:40:00
+@Title : solve use case 2
 """
 
 print("welcome in Address Book")
@@ -11,16 +11,19 @@ print("welcome in Address Book")
 
 class Contact:
 
-    def __init__(self, firstname, lastnames, address, city, state, zipcode, phonenumber, email):
-        self.firstname = firstname
-        self.lastnames = lastnames
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.phonenumber = phonenumber
-        self.email = email
+    def __init__(self):
+        """
+        desc: Add contact details using console
+        """
+        self.firstname = input("Enter first name: ")
+        self.lastname = input("Enter last name: ")
+        self.address = input("Enter address: ")
+        self.city = input("Enter city: ")
+        self.state = input("Enter state: ")
+        self.zipcode = input("Enter zipcode: ")
+        self.phonenumber = input("Enter phone number: ")
+        self.email = input("Enter email: ")
 
 
 if __name__ == '__main__':
-    Contact("vishal", "Patil", "Limb", "Limb", "Maharashtra", "416312", "8329235763", "Vk@gmail.com")
+    Contact()
